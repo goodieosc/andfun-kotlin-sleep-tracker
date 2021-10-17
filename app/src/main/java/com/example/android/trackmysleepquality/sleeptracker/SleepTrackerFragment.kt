41972d4673fe3fbbfe130eb9e25a6423c75baa56
oscar <oscar.goodwin@gmail.com>
2021-10-17 15:20:48 +0800

    Change span size of header

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -72,6 +72,13 @@ class SleepTrackerFragment : Fragment() {
 
         //Grid layout
         val manager = GridLayoutManager(activity,3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(position: Int) = when (position){
+                0 -> 3
+                else -> 1
+            }
+
+        }
         binding.sleepList.layoutManager = manager
 
         val adapter = SleepNightAdapter(SleepNightListener {
